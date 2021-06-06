47bb3c18176e6840f00793647ed87ad9bb9703f1
chortas <chortas@fi.uba.ar>
2021-06-06 16:21:12 -0300

    Small refactor + save in queue result

--- reducer_join/reducer_join.py
+++ reducer_join/reducer_join.py
@@ -15,7 +15,7 @@ class ReducerJoin():
         self.matches_and_players = {}
         self.matches = set()
         self.players = set()
-        self.reset_counter = 2 # to know when to stop
+        self.len_join = 2 # to know when to stop
         # add data structure to save joins
 
     def start(self):
@@ -26,6 +26,8 @@ class ReducerJoin():
         create_exchange(channel, self.join_exchange, exchange_type="direct")
         queue_name = create_and_bind_anonymous_queue(channel, self.join_exchange, 
         routing_keys=[self.match_consumer_routing_key, self.player_consumer_routing_key])
+
+        create_queue(channel, self.grouped_result_queue)
 
         self.__consume_matches_and_players(channel, queue_name)
 
@@ -40,12 +42,16 @@ class ReducerJoin():
         ch.basic_ack(delivery_tag=method.delivery_tag)
         body_parsed = json.loads(body) 
         if len(body_parsed) == 0:
-            self.reset_counter -= 1
-            if self.reset_counter == 0:
-                logging.info("[REDUCER_JOIN] The client already sent all messages")
-                self.__print_join()
-            return
+            return self.__handle_end_join(ch)
+        self.__store_matches_and_players(body_parsed, method)
+    
+    def __handle_end_join(self, ch):
+        self.len_join -= 1
+        if self.len_join == 0:
+            logging.info("[REDUCER_JOIN] The client already sent all messages")
+            self.__send_to_grouped_queue(ch)
 
+    def __store_matches_and_players(self, body_parsed, method):
         id_to_join = (body_parsed[self.match_id_field] 
                     if method.routing_key == self.match_consumer_routing_key
                     else body_parsed[self.player_match_field])
@@ -58,9 +64,10 @@ class ReducerJoin():
         if method.routing_key == self.player_consumer_routing_key:
             self.players.add(id_to_join)
             self.matches_and_players[id_to_join].append(body_parsed)
-    
-    def __print_join(self):
+
+    def __send_to_grouped_queue(self, ch):
         logging.info(f"Matches and players: {self.matches_and_players}")
         for token in self.matches_and_players:
             if token in self.matches and token in self.players:
-                logging.info(f"Una fila joineada: {self.matches_and_players[token]}")
+                players = self.matches_and_players[token] 
+                send_message(ch, json.dumps(players), queue_name=self.grouped_result_queue)
